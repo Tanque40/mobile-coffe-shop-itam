9d9aa7bb080e53a10e45a6e75301f2935bbc91d6
Bruno Vitte <bruno_vitte@hotmail.com>
2022-05-11 15:23:49 -0500

    before add xmlhttprequest

--- api/ApiConnection.js
+++ api/ApiConnection.js
@@ -1,40 +1,34 @@
-const link = "https://coffeshopitam.herokuapp.com/"
-var request = new XMLHttpRequest()
+const link = 'https://coffeshopitam.herokuapp.com/';
+var request = new XMLHttpRequest();
 
-export function logInApi( user, password ){
-    var formData = new FormData();
-// Parameters: {"authenticity_token"=>"xzaY1fCiCfqGEiq4BLn1IOIK8WnBEJ7/pCgWtU+2uLO3vJdjZUNm4AyviBKsl5raLrhGqgZcqHy5WL7QNjRiAg==", "user"=>{"email"=>"Bruno@itamcoffe.mx", "password"=>"[FILTERED]", "remember_me"=>"0"}, "commit"=>"Log in"}
+export function logInApi(user, password) {
+  var formData = new FormData();
+  // Parameters: {"authenticity_token"=>"xzaY1fCiCfqGEiq4BLn1IOIK8WnBEJ7/pCgWtU+2uLO3vJdjZUNm4AyviBKsl5raLrhGqgZcqHy5WL7QNjRiAg==", "user"=>{"email"=>"Bruno@itamcoffe.mx", "password"=>"[FILTERED]", "remember_me"=>"0"}, "commit"=>"Log in"}
 
-    formData.append(
-        "Parameters",
-        {
-            "user": {
-                "email": user,
-                "password": password,
-                "remember_me": 0
-            }
-        }
-    );
-    
-    request.open('POST', link + "users/sign_in");
-    var data = request.onload = () => {
-        // Begin accessing JSON data here
-        var data = JSON.parse(this.response)
+  formData.append('Parameters', {
+    user: {
+      email: user,
+      password: password,
+      remember_me: 0,
+    },
+  });
 
-        if (request.status >= 200 && request.status < 400) {
-            
-            data.forEach(movie => {
-                console.log(movie)
+  request.open('POST', link + 'api/v1/users/sign_in');
+  var data = (request.onload = () => {
+    // Begin accessing JSON data here
+    //data = JSON.parse(this.response);
 
-            })
-            return data
-        } else {
-            console.log('error')
-        }
+    if (request.status >= 200 && request.status < 400) {
+      data.forEach(movie => {
+        console.log(movie);
+      });
+      return data;
+    } else {
+      console.log('error');
     }
+  });
 
-    request.send(formData);
-    
-    return data;
+  request.send(formData);
 
+  return data;
 }
--- src/screens/LogIn/components/LogIn.js
+++ src/screens/LogIn/components/LogIn.js
@@ -1,84 +1,67 @@
 // Basics
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 //Redux
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 // Functions of React-Native
-import { StyleSheet, TouchableOpacity } from 'react-native'
+import {StyleSheet, TouchableOpacity} from 'react-native';
 
 // Design
-import { 
-    Text,
-    Layout,
-    Button
-} from '@ui-kitten/components';
+import {Text, Layout, Button} from '@ui-kitten/components';
 
 // Components
 import LogInForm from './LogInForm';
 
 class LogIn extends Component {
-    signUp = () => {
-        this.props.dispatch({
-            type: 'NEW_USER',
-            payload: {
-            }
-        })
-    }
+  signUp = () => {
+    this.props.dispatch({
+      type: 'NEW_USER',
+      payload: {},
+    });
+  };
 
-    render() {
-        return(
+  render() {
+    return (
+      <Layout style={styles.container}>
+        <Layout level="2" style={styles.form}>
+          <Text category="h2"> Coffe Shop ITAM </Text>
+          <Text category="h3"> Log In </Text>
 
-            <Layout style={styles.container}>
-                
-                <Layout
-                    level = '2'
-                    style={styles.form}
-                >
+          <LogInForm />
 
-                    <Text category='h2'> Coffe Shop ITAM </Text>
-                    <Text category='h3'> Log In </Text>
-                    
-                    <LogInForm />
-                    
-                    <Layout>
-                        <TouchableOpacity onPress={this.signUp} >
-                            <Button appearance='ghost'>
-                                Sign Up
-                            </Button>
-                        </TouchableOpacity>
-                    </Layout>
-                    
-                </Layout>
-
-            </Layout>
-
-        );
-    }
-
+          <Layout>
+            <TouchableOpacity onPress={this.signUp}>
+              <Button appearance="ghost">Sign Up</Button>
+            </TouchableOpacity>
+          </Layout>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-    form: {
-        flex: 1,
-        padding: 15,
-        justifyContent: 'center',
-    },
-    signUpContainer: {
-        paddingVertical: 10,
-        justifyContent: "center",
-        backgroundColor: "#3366ff",
-        width: 150,
-    }
+  container: {
+    flex: 1,
+  },
+  form: {
+    flex: 1,
+    padding: 15,
+    justifyContent: 'center',
+  },
+  signUpContainer: {
+    paddingVertical: 10,
+    justifyContent: 'center',
+    backgroundColor: '#3366ff',
+    width: 150,
+  },
 });
 
-function mapStateToProps(state){
-    return {
-        state 
-    }
+function mapStateToProps(state) {
+  return {
+    state,
+  };
 }
 
 export default connect(mapStateToProps)(LogIn);
